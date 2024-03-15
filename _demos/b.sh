183443014c8dd5d3f9090b32e0e7e39ebf632266
Allen Guo <guosonglin001@gmail.com>
2024-03-15 02:26:46 +0000

    wip

--- _demos/b.sh
+++ _demos/b.sh
@@ -3,11 +3,17 @@
 
 # TODO: `picolibc-cross.txt` come from project `crosstool-ng`, needs update
 
-meson setup --reconfigure \
-  --cross-file=picolibc-cross.txt \
-  --prefix=$PWD/build-meson/install \
-  -Dsemihost=false \
+args=(
+  --reconfigure
+  --cross-file=picolibc-cross.txt
+  --prefix=$PWD/build-meson/install
+  # -Dsemihost=false
+  -Dsemihost=true
+  # -Dnewlib-initfini-array=false
+  -Dnewlib-initfini-array=true
   build-meson
+)
+meson setup "${args[@]}"
 
 meson compile -C build-meson
 meson install -C build-meson
@@ -16,11 +22,10 @@ meson install -C build-meson
 
 pushd build-meson/install/lib/
 riscv64-unknown-elf-objdump -d libc.a >libc.a.dasm
-riscv64-unknown-elf-objdump -d -g libc.a >libc.a.g.dasm
 riscv64-unknown-elf-objdump -d libcrt0-hosted.a >liblibcrt0-hostedc.a.dasm
 riscv64-unknown-elf-objdump -d libcrt0-minimal.a >libcrt0-minimal.a.dasm
+riscv64-unknown-elf-objdump -d libcrt0-semihost.a >libcrt0-semihost.a.dasm
 riscv64-unknown-elf-objdump -d libcrt0.a >libcrt0.a.dasm
-riscv64-unknown-elf-objdump -d libg.a >libg.a.dasm
-riscv64-unknown-elf-objdump -d libm.a >libm.a.dasm
 riscv64-unknown-elf-objdump -d libdummyhost.a >libdummyhost.a.dasm
+riscv64-unknown-elf-objdump -d -g libc.a >libc.a.g.dasm
 popd
---
+++ _demos/t2.hello.sh
@@ -0,0 +1,39 @@
+TARGET="hello-world/hello-world.c"
+# TARGET="hello-world/printf.c"
+
+OUTDIR="_demos/outdir"
+SPECS="build-meson/picolibc.specs"
+LDSCRIPT="hello-world/riscv.ld"
+
+args=(
+  -march=rv64imac -mabi=lp64
+  -specs=$SPECS
+  -mcmodel=medany
+  # -g
+  -O0
+  # -v
+  --oslib=semihost
+  -T$LDSCRIPT
+  $TARGET
+)
+
+mkdir -p $OUTDIR
+riscv64-unknown-elf-gcc -S -o $OUTDIR/a.s "${args[@]}"
+riscv64-unknown-elf-gcc -o $OUTDIR/a.out -Wl,-Map=$OUTDIR/map.map "${args[@]}"
+
+riscv64-unknown-elf-objdump -d $OUTDIR/a.out >$OUTDIR/a.out.dasm
+riscv64-unknown-elf-objdump -d -g $OUTDIR/a.out >$OUTDIR/a.out.g.dasm
+
+# =============================================================================
+
+args=(
+  -machine virt
+  -cpu rv64
+  -semihosting-config enable=on
+  -nographic
+  -bios none
+  -monitor none
+  -serial none
+  -kernel $OUTDIR/a.out
+)
+qemu-system-riscv64 "${args[@]}"
