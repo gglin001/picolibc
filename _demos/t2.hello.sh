e587829e991590d4722810e954c28eb4cd2e350f
Allen Guo <guosonglin001@gmail.com>
2024-05-18 23:59:05 +0800

    wip

--- _demos/t2.hello.sh
+++ _demos/t2.hello.sh
@@ -49,7 +49,7 @@ args=(
   -l
   _demos/outdir/a.out
 )
-spike -l --log log.log "${args[@]}"
-# spike -l "${args[@]}" 2>log.log
+spike -l --log _demo/log.log "${args[@]}"
+# spike -l "${args[@]}" 2>_demo/log.log
 # spike -d "${args[@]}"
 # spike "${args[@]}"
