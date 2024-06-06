e4d836de89ffdfe5c28a155fd28897583af0687a
Allen Guo <guosonglin001@gmail.com>
2024-06-06 14:10:50 +0800

    wip

--- _demos/t2.hello.sh
+++ _demos/t2.hello.sh
@@ -1,10 +1,11 @@
-TARGET="hello-world/hello-world.c"
-# TARGET="hello-world/printf.c"
+###############################################################################
 
-OUTDIR="_demos/outdir"
+# TARGET="hello-world/printf.c"
+TARGET="hello-world/hello-world.c"
 SPECS="build-meson/picolibc.specs"
 LDSCRIPT="_demos/riscv.ld"
 
+DIR="_demos/t2.hello" && mkdir -p $DIR
 args=(
   # -march=rv64im -mabi=lp64
   -march=rv64imd -mabi=lp64d
@@ -18,40 +19,38 @@ args=(
   -T$LDSCRIPT
   $TARGET
 )
+riscv64-unknown-elf-gcc -S -o $DIR/a.s "${args[@]}"
+riscv64-unknown-elf-gcc -o $DIR/a.out -Wl,-Map=$DIR/map.map "${args[@]}"
 
-mkdir -p $OUTDIR
-riscv64-unknown-elf-gcc -S -o $OUTDIR/a.s "${args[@]}"
-riscv64-unknown-elf-gcc -o $OUTDIR/a.out -Wl,-Map=$OUTDIR/map.map "${args[@]}"
-
-riscv64-unknown-elf-objdump -d $OUTDIR/a.out >$OUTDIR/a.out.dasm
-riscv64-unknown-elf-objdump -d -g $OUTDIR/a.out >$OUTDIR/a.out.g.dasm
+riscv64-unknown-elf-objdump -d $DIR/a.out >$DIR/a.out.dasm
+riscv64-unknown-elf-objdump -d -g $DIR/a.out >$DIR/a.out.g.dasm
 
 ###############################################################################
 
-# semihost
+DIR="_demos/t2.hello" && mkdir -p $DIR
 args=(
   -machine virt
   -cpu rv64
-  -semihosting-config enable=on
+  -semihosting-config enable=on # semihost
   -nographic
   -bios none
   -monitor none
   -serial none
-  -kernel $OUTDIR/a.out
+  -kernel $DIR/a.out
 )
 qemu-system-riscv64 "${args[@]}"
 
 ###############################################################################
 
-# debug
+# spike does not support semihost, check log for insts
+DIR="_demos/t2.hello" && mkdir -p $DIR
 args=(
   --pc=0x80000000
+  # -d
   -l
-  _demos/outdir/a.out
+  --log $DIR/a.out.spike.log
+  $DIR/a.out
 )
-spike -l --log _demo/log.log "${args[@]}"
-# spike -l "${args[@]}" 2>_demo/log.log
-# spike -d "${args[@]}"
-# spike "${args[@]}"
+spike "${args[@]}"
 
 ###############################################################################
